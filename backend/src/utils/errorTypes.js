e34dd8abcee4b0bd08a17cc64c747c35ef3f44d8
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-14 17:37:40 -0600

    Add errorTypes module

---
+++ backend/src/utils/errorTypes.js
@@ -0,0 +1,28 @@
+const errorTypes = {
+  NotFoundError: {
+    message: "Recursos no encontrados",
+    statusCode: 404,
+  },
+  ForbiddenError: {
+    message: "Prohibido",
+    statusCode: 403,
+  },
+  UnauthorizedError: {
+    message: "No autorizado",
+    statusCode: 401,
+  },
+  ValidationError: {
+    message: "Datos no validos",
+    statusCode: 400,
+  },
+  InternalServerError: {
+    message: "Error interno del servidor",
+    statusCode: 500,
+  },
+  ServiceUnavailableError: {
+    message: "Servicio no disponible",
+    statusCode: 503,
+  },
+};
+
+module.exports = errorTypes;
